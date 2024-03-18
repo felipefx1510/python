ffcf881c3d31b05f74327d1f8e6a4699ea12a838
Felipe <felipefelixsdl@gmail.com>
2024-03-17 21:53:12 -0300

    função time

--- aprendendo_python_cond/01_adivinhação.py
+++ aprendendo_python_cond/01_adivinhação.py
@@ -1,8 +1,12 @@
 import random
+import time
 
 num_g = random.randint(1,5)
 
 num_u = int(input("Digite um número entre 1 e 5: "))
+
+print("Processando...")
+time.sleep(3)
 
 if num_g == num_u:
     print("Parabéns, você acertou!")
